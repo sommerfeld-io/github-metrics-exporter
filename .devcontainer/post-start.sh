a57c583251991e80fd48b9e263842591dc90c53d
Sebastian Sommerfeld <101367275+sebastian-sommerfeld-io@users.noreply.github.com>
2026-06-09 22:09:39 +0200

    Initial commit

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+## Use nftables backend for iptables to ensure Docker networking works on Ubuntu 26.04+
+## <https://github.com/orgs/sommerfeld-io/projects/1/views/1?pane=issue&itemId=184218287>
+sudo update-alternatives --set iptables /usr/sbin/iptables-nft
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
+
+task cleanup:update
